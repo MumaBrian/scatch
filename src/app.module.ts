cdd26fcec0e88d149c85b5d466fe474f7a00d525
Muma Brian <muma.neoworlder@gmail.com>
2024-04-05 16:02:06 +0100

    Finished CRUD for messages

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MessagesController } from './messages/messages.controller';
 import { MessagesModule } from './messages/messages.module';
+import { MessagesService } from './messages/messages.service';
+import { MessagesRepository } from './messages/mesages.repository';
 
 @Module({
   imports: [MessagesModule],
   controllers: [AppController, MessagesController],
-  providers: [AppService],
+  providers: [AppService, MessagesService, MessagesRepository],
 })
 export class AppModule {}
--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,21 +1,25 @@
 /* eslint-disable prettier/prettier */
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { createMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
+
+    constructor (public messagesService: MessagesService ) {}
+
     @Get()
     listMessages() {
-
+        return this.messagesService.findAll();
     }
 
     @Post()
     createMessage(@Body() body:createMessageDto) {
-        console.log(body);
+        return this.messagesService.create(body.content)
     }
 
     @Get('/:id')
     getMessage(@Param('id') id: string) {
-        console.log(id);
+        return this.messagesService.findOne(id);
     }
 }
